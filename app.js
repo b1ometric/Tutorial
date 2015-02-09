df2a9d42a42a6834c79c75bfb9c156331e18815e
b1ometric <glenn@wildons.com>
2015-02-09 10:42:11 +0000

    step_6

--- app.js
+++ app.js
@@ -39,13 +39,34 @@ angular.module("socially").controller("PartiesListCtrl", ['$scope', '$meteorColl
 
 }]);
   
-angular.module("socially").controller("PartyDetailsCtrl", ['$scope', '$stateParams',
-  function($scope, $stateParams){
+angular.module("socially").controller("PartyDetailsCtrl", ['$scope', '$stateParams', '$meteorObject',
+  function($scope, $stateParams, $meteorObject){
 
-    $scope.partyId = $stateParams.partyId;
+    $scope.party = $meteorObject(Parties, $stateParams.partyId);
 
+/*
+    //TODO: Neither of these save functions work
+    
+    $scope.party = $meteorObject(Parties, $stateParams.partyId, false);
+    
+    //$scope.save = function() {
+    //  $scope.party.save();
+    //};
+
+    $scope.save = function() {
+      $scope.party.save().then(function(numberOfDocs){
+        console.log('save success doc affected ', numberOfDocs);
+      }, function(error){
+        console.log('save error', error);
+      });
+    };
+
+    $scope.reset = function() {
+      $scope.party.reset();
+    };
+*/
 }]);
-
+  
 if (Meteor.isServer) {
   Meteor.startup(function () {
     if (Parties.find().count() === 0) {
