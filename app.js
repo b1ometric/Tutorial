ef89cfb5d5947b8d20e98896c6955ec4f56c60c3
b1ometric <glenn@wildons.com>
2015-02-09 10:16:25 +0000

    step_5

--- app.js
+++ app.js
@@ -2,8 +2,28 @@ Parties = new Mongo.Collection("parties");
 
 if (Meteor.isClient) {
 
-angular.module('socially',['angular-meteor']);
+angular.module('socially',['angular-meteor', 'ui.router']);
 
+angular.module("socially").config(['$urlRouterProvider', '$stateProvider', '$locationProvider',
+  function($urlRouterProvider, $stateProvider, $locationProvider){
+
+    $locationProvider.html5Mode(true);
+
+    $stateProvider
+      .state('parties', {
+        url: '/parties',
+        templateUrl: 'parties-list.tpl',
+        controller: 'PartiesListCtrl'
+      })
+      .state('partyDetails', {
+        url: '/parties/:partyId',
+        templateUrl: 'party-details.tpl',
+        controller: 'PartyDetailsCtrl'
+      });
+
+      $urlRouterProvider.otherwise("/parties");
+}]);  
+  
 angular.module("socially").controller("PartiesListCtrl", ['$scope', '$meteorCollection',
   function($scope, $meteorCollection){
 
@@ -16,6 +36,13 @@ angular.module("socially").controller("PartiesListCtrl", ['$scope', '$meteorColl
     $scope.removeAll = function(){
       $scope.parties.remove();
     };
+
+}]);
+  
+angular.module("socially").controller("PartyDetailsCtrl", ['$scope', '$stateParams',
+  function($scope, $stateParams){
+
+    $scope.partyId = $stateParams.partyId;
 
 }]);
 
@@ -38,3 +65,5 @@ if (Meteor.isServer) {
     }
    });
 }
+  
+}  
